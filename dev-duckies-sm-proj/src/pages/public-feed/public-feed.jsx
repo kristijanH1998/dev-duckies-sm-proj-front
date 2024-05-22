87eff370f74999317592b594b98c9b75cf09e779
kristijanH1998 <kristijan.hornung1@gmail.com>
2024-05-22 03:16:49 -0700

    test requests with cookies enabled

--- dev-duckies-sm-proj/src/pages/public-feed/public-feed.jsx
+++ dev-duckies-sm-proj/src/pages/public-feed/public-feed.jsx
@@ -1,6 +1,17 @@
 import Header from "../../app-wide-components/Navbar/MobileNav.jsx";
 import Post from "../../app-wide-components/post.jsx";
 import "../../index.css";
+import axios from 'axios'
+
+axios.defaults.withCredentials = true;
+axios.post('http://localhost:8080/posts', {
+  "post_content": "This is a test post."
+}).then(res => {
+    axios.get('http://localhost:8080/feed/1').then(res => {
+    console.log(res.data)
+  }).catch(error => {console.log(error.response.data.error)})
+}).catch(error => {console.log(error.response.data.error)})
+ 
 
 function PublicFeed() {
   return (
