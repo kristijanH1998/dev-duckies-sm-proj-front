deab951b2e2c617b8e68a9550f54d2416481892d
kristijanH1998 <kristijan.hornung1@gmail.com>
2024-05-22 04:04:03 -0700

    Post components rendered with .map() from fetched result

--- dev-duckies-sm-proj/src/pages/public-feed/public-feed.jsx
+++ dev-duckies-sm-proj/src/pages/public-feed/public-feed.jsx
@@ -2,41 +2,34 @@ import Header from "../../app-wide-components/Navbar/MobileNav.jsx";
 import Post from "../../app-wide-components/post.jsx";
 import "../../index.css";
 import axios from 'axios'
+import { useState, useEffect } from "react";
 
 axios.defaults.withCredentials = true;
-axios.post('http://localhost:8080/posts', {
-  "post_content": "This is a test post."
-}).then(res => {
-    axios.get('http://localhost:8080/feed/1').then(res => {
-    console.log(res.data)
-  }).catch(error => {console.log(error.response.data.error)})
-}).catch(error => {console.log(error.response.data.error)})
- 
 
 function PublicFeed() {
+  
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/feed/1')
+    .then(res => setPosts(res.data))
+    .catch(err => console.log(err));
+  }, [])
+
   return (
     <div className="App">
       <Header />
       <div className="columns">
         <div className="column is-12">
-          <Post
-            username="Tommy"
-            postContent="Testing 123"
-            date="10/3/24"
-            time="13:14"
-          />
-          <Post
-            username="Jessie"
-            postContent="Testing 123"
-            date="10/3/24"
-            time="13:14"
-          />
-          <Post
-            username="Albert"
-            postContent="Testing 123"
-            date="10/3/24"
-            time="13:14"
-          />
+          {posts.map(post => (
+            <Post
+              key={post.id}
+              username= {post.username}
+              postContent={post.content}
+              date={post.timestamp}
+              time={post.timestamp}
+            />
+          ))}
         </div>
       </div>
     </div>
