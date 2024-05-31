929eaf0252d3c67a2b5e59847fdac532e23c6e9d
vlomeli <victorlomeli2023@gmail.com>
2024-05-30 17:01:45 -0700

    WIP added pagination to the feed
    Still might need minor tweaks with responsiveness on a smaller application.

--- dev-duckies-sm-proj/src/pages/public-feed/public-feed.jsx
+++ dev-duckies-sm-proj/src/pages/public-feed/public-feed.jsx
@@ -8,14 +8,32 @@ import { useState, useEffect } from "react";
 axios.defaults.withCredentials = true;
 
 function PublicFeed() {
-  
   const [posts, setPosts] = useState([]);
+  const [page, setPage] = useState(1);
+
+  const increasePage = () => {
+    if (posts.length === 5) {
+      setPage(page + 1);
+    }
+    console.log(userInfo);
+  };
+
+  const decreasePage = () => {
+    if (page > 1) {
+      setPage(page - 1);
+    }
+  };
 
   useEffect(() => {
-    axios.get('http://localhost:8080/feed/1')
-    .then(res => setPosts(res.data))
-    .catch(err => {return err.code});
-  }, [posts])
+    if (page > 0) {
+      axios
+        .get(`http://localhost:8080/feed/${page}`)
+        .then((res) => setPosts(res.data))
+        .catch((err) => {
+          return err.code;
+        });
+    }
+  }, [posts, page]);
 
   return (
     <div className="App">
@@ -23,22 +41,38 @@ function PublicFeed() {
       <div className="columns">
         <div className="column is-12">
           <div className="box scroll-container">
-          {posts.slice(0, 5).map((post) => (
-            <Post
-              key={post.id}
-              username={post.username}
-              postContent={post.content}
-              date={post.timestamp.substr(0, 10)}
-              time={post.timestamp.substr(11, 8)}
-              profilePic={post.profilePic}
-              likeCount={post.likeCount}
-              commentCount={post.commentCount}
-              id={post.id}
-            />
-          ))}
+            {posts.map((post) => (
+              <Post
+                key={post.id}
+                username={post.username}
+                postContent={post.content}
+                date={post.timestamp.substr(0, 10)}
+                time={post.timestamp.substr(11, 8)}
+                profilePic={post.profilePic}
+                likeCount={post.likeCount}
+                commentCount={post.commentCount}
+                id={post.id}
+              />
+            ))}
+          </div>
+          <div className="pagination-controls">
+            <button
+              className="button is-ghost mt-1"
+              onClick={decreasePage}
+              disabled={page === 1}
+            >
+              Previous
+            </button>
+            <button
+              className="button is-ghost mt-1"
+              onClick={increasePage}
+              disabled={posts.length < 5}
+            >
+              Next
+            </button>
+          </div>
         </div>
       </div>
-    </div>
     </div>
   );
 }
