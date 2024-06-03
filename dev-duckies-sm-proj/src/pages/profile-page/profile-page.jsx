59c0df48cc23cf0f14da02073d4149343bf8904f
kristijanH1998 <kristijan.hornung1@gmail.com>
2024-06-03 00:03:42 -0700

    user info object can now be updated by filling out the update form on frontend

--- dev-duckies-sm-proj/src/pages/profile-page/profile-page.jsx
+++ dev-duckies-sm-proj/src/pages/profile-page/profile-page.jsx
@@ -50,12 +50,16 @@ export default function ProfilePage() {
     setIsEditing(true); // Activate editing mode when the user info is clicked
   };
   const handleUserInfoChange = (event) => {
-    setNewUserInfo(event.target.value);
+    if(event.target.value.trim() != '') {
+      setNewUserInfo(newUserInfo => ({...newUserInfo, [event.target.id]: event.target.value}));
+    }        
   };
 //Upon submitting user info it is saved to database and IsEditingUser is set to false.
   const handleUserInfoSubmit = (event) => {
     event.preventDefault();
     setIsEditing(false);
+    console.log(newUserInfo)
+    setNewUserInfo({});
     alert('User info saved!') // Deactivate editing mode when the form is submitted
     // Add logic here to save the updated user info to the backend
   };
@@ -89,7 +93,7 @@ useEffect(() => {
               src={`data:image/png;base64,${userInfo.profile_picture ? userInfo.profile_picture : "iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAMAAACdt4HsAAAACXBIWXMAAA7EAAAOxAGVKw4bAAAC6FBMVEVHcExPkNwAAAAJEBkDBgksUHpyp+dRk+Fro+Zxp+dyqOcgO1sBAgQAAAByp+cGDBJIg8hNjNYVJztLiM8AAAAYLEQCBQh0qOdyp+cAAQJameMzXY5noeUMFiIaMEpameMPHCtHgcZTk99upeZIg8kAAAAIEBpooeU4ZpxIg8gAAABzqOd0qOdwpudooeUAAAAyW4sAAAB0qOcvVYJ0qOd0qOcKFB40Xo8AAABzqOd0qOdwpuZyqOdupeYlQ2Z0qOcCAwVMitMvVoMvVYNyqOcAAAAAAAAAAAB0qOdyp+dMitMAAAB0qOdzp+cAAAByp+cCBQgAAAByp+dzqOc6aqIAAAEAAAA6aqIHDhYQHCwPHCtPj9pro+ZhneRinuQjP2EQHS0OGylem+M1X5JdmuNpouVYmONYl+IhPV1inuRjnuQ9b6ooSW88bqgzXI0kQmVGgMNHgMRzqOd0qOcZLUVzqOd0qehzqOdzqOdxp+dzqOdyp+d0qOdzqOdzqOZLh89zp+cBAwUCBQgBAwX///9SlOJCdrVem+RTlOJ0qOdUleJWluJamONtpOZgnORKhs6Lt+pIg8hMitP4+vzv8/hRk+GJtutloOWWvu2Kt+u61PNkn+VameNZmONbmeP+/v7i7fpspOagxO9yqOf+//+dwu6z0PL2+f3d6vns8/uGtOpHgMRpouZso+ZtlcVqo+ZHgcVupeZ5nsp/os1xpufC2fRxp+dZh77X5vhOjtnj6/RIgsheltlQkNzr8vtrlMVNidFOjdjW4e/z9vpKhcxGfsFulsawzO1Qkd5/qtxEd7VPjtpLh85ooeX3+v1rlcdPj9tNitSgutr9/v5PgLpzmcjc5fH5+vxfnOSCsurT5PeEs+rS4/fL3/bn8PvK3vbm7/qXvu1gneRinuRTleJineS81fNbmuNEebrZ4/CLq9GMq9Lg6fPZ5PBEeLfg6POpwN1FeLdeisBfi8Cov9xDebpEe7xEerygdt79AAAAhHRSTlMA9yxFCZoY/hgEYH0LFT1A4/Ji6h9qHt4sGv6nSAMInhafp0fiMgrmt+EwjN2E9jOqJqeg9o4ysC6E5xJG7ImeNe6jorEBKhPsEu8QshIj8AIF9PC+Lw6/OlNV9pdub1kzM4xQi5eop1rc3XQjdFAjjo6YlmtJ/m9tjkjejxIS6S0UFBqBOXA9AAADkUlEQVRYw2NgGAW0BGxVFdr2Rkb22uWlbCRr5rQp1inr3Na7fs2a9b1bOst0imw4SbHb0qpz7eM5Xa1Q8GTO07WdVpbEusPX1GTjI7hmGOjq22hSU0uMfi6P3uetWMG8Xg8ugtrrfLa1t+IE7Vt8fPHrlxA/29WKB3SdFW/Ap58vuK+VAOgL5sOjP2heK0EwLwinCRL+c9FVL1y1aiG62Fx/Cez6PavnoCmdtqAFCBbMRBPuqfbEaoALevAvW9ICBkuWoUeGC9b4n46mbGkLHCxFk5qNJT1Y23agWbMcYcByNMd12FpjGFDZg2bLwxYkMAM9ICsx8o8eemDPQjZgFrqsHnrO0u1GV7IV2YCt6LLdumj5Xx0jwRxHNuA4hrQ6avmg3IOh4iSyAScxpHuUUQyIwUyyB94g9L8/gCmfimJAFqaCE+cnw/RPPn8CUz4TWT+HFJZc0391CkT/lKv9WKSlOJAMkOnAomLqxCuXrl2/fu3SlYlTsWVLGSQD4rDn3Enf2oDg+yTssrFIBiThyPtTL1y8eGEqDskUJAPkW8kA8kgGpJFjQDqSAUpY5GfPmLVj37nLl8/t2/FsxnQsCpSQDFDEkL15Zz5ySpx/6zaGEkUkAwrQ5G7cb8EAd9EL7EIkA3LRXP+gBQu4h+aPbCQD8lCz6ooWrGAFapZPRjIgnx1ZZlELDrAIWRV7DpIBcirIBd5KXAasRE7xmnLIuUkaSWZxC06wGEmZNEp2zkCuTnAbMA1JWTyKAQrCCJkJuA2YgFAlrIBigCwrqQawyqKWqomMpBnAmIBWrKs5weX2TMGlf8oeuCJnNfSaxcwLJndw/2Hs+g/vPwhT42iGUbWxcMNNP/LpJTb9bz8fgSvhZsGsXcXC4dJn3r3aha591+uPZ+AKQsSwVO8BEQJwBaePth37cghRrB/6cKzt6Gm4tEBEALYGAk+kICKWdu5+0XZq+7pNmzdvWrf9VNvX3TsRcoKRPNjbOI1C0ci5Ze+k/okbVq/eMLF/0l5k8WihJlytLEkhQcJFoaBQM+52nmSUACH9AlGS+FqavPwh+PWH8/Pib+uGhXI74tbuxR0aRqi1HejH78yIXTujE79fIBHtfW8mZtZ6TO3CrMxM3sT1OFzd3RxEDTSRykl2FQNRBzd3V+I7PTwiFvrMonbG5hoa5sZ2Jcz6FmI8pPa7WHhFDJm0VFW1mAxFeFlGO7F4AADORa3iNYFJbAAAAABJRU5ErkJggg=="}`}
               alt="Profile picture" />
             </figure>
-          <button className= "button is-small mt-0 p-0">
+          <button className= "button is-small mt-3 p-0">
             <label className="" htmlFor="input-file" style={{border:"none"}}>
             <input className="file-input" id= "input-file" type="file" accept="image/jpeg, image/png, image/jpg"/>
             <span className="file-cta" style={{border:"none"}}>
@@ -110,8 +114,9 @@ useEffect(() => {
               <div id="text-area">
               <form onSubmit={handleUserInfoSubmit}>
                 <textarea
-                className= "edit-textarea"
-                  value={newUserInfo}
+                  id="first_name"
+                  className= "edit-textarea"
+                  value={newUserInfo.first_name}
                   onChange={handleUserInfoChange}
                   rows={4}
                   cols={50}
@@ -119,8 +124,9 @@ useEffect(() => {
                   placeholder='First Name:'
                 />
                 <textarea
-                className= "edit-textarea"
-                  value={newUserInfo}
+                  id="last_name"
+                  className= "edit-textarea"
+                  value={newUserInfo.last_name}
                   onChange={handleUserInfoChange}
                   rows={4}
                   cols={50}
@@ -128,8 +134,9 @@ useEffect(() => {
                   placeholder='Last Name:'
                 />
                 <textarea
-                className= "edit-textarea"
-                  value={newUserInfo}
+                  id="country"
+                  className= "edit-textarea"
+                  value={newUserInfo.country}
                   onChange={handleUserInfoChange}
                   rows={4}
                   cols={50}
@@ -137,8 +144,9 @@ useEffect(() => {
                   placeholder='Country:'
                 />
                 <textarea
-                className= "edit-textarea"
-                  value={newUserInfo}
+                  id="date_of_birth"
+                  className= "edit-textarea"
+                  value={newUserInfo.date_of_birth}
                   onChange={handleUserInfoChange}
                   rows={4}
                   cols={50}
@@ -146,8 +154,9 @@ useEffect(() => {
                   placeholder='Birthday (YYYY-MM-DD):'
                 />
                 <textarea
-                className= "edit-textarea"
-                  value={newUserInfo}
+                  id="biography"
+                  className= "edit-textarea"
+                  value={newUserInfo.biography}
                   onChange={handleUserInfoChange}
                   rows={4}
                   cols={50}
